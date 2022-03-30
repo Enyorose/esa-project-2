30c23b5284c968c28de68d0efec0759bd0d68921
Jessica Watson <jessica.watson1@live.com>
2022-03-29 22:27:29 -0600

    populated navlinks

---
+++ components/TheNavBar.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+    <NuxtLink
+      v-for="navLink in navLinks"
+      :key="navLink.id"
+      :to="navLink.path"
+      >{{ navLink.name }}</NuxtLink>
+      </div>
+</template>
+<script setup>
+const navLinks= [
+    {
+        name: 'Home',
+        path: '/'
+    },
+    {
+        name: 'Team',
+        path: '/'
+    },
+    {
+        name: 'Social',
+        path: '/'
+    },
+    {
+        name: 'Sponsor',
+        path: '/'
+    },
+    {
+        name: 'Contact',
+        path: '/'
+    },
+    {
+        name: 'Events',
+        path: '/'
+    }
+]
+</script>
